8544e2de42fd5ccce11212e5a039326bf7d3c78e
MrChenGG <18977939601m0@sina.cn>
2020-07-17 12:19:54 +0800

    配置

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  publicPath: './',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common', 'components': '@/components', 'network': '@/network', 'views': '@/views', 'plugins': '@/plugins',
+      }
+    }
+  }
+}
